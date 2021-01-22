5c7663b1412f6dce8a5e265ba1727abeb981f61b
John Loughran <johnpdloughran@gmail.com>
2021-01-22 16:26:48 +0000

    Uses passport,local,mongoose and exp-session, cookies

--- app.js
+++ app.js
@@ -6,20 +6,40 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
+const session = require("express-session");            // PASSPORT not sessions!
+const passport = require("passport");     // autheniticate()                          // PASSPORT
+const passportLocalMongoose = require("passport-local-mongoose");   // PASSPORT
+// register(), login(), logout()
+
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+  secret: "OurLittleSecret!Temporary",
+  resave: false,
+  saveUninitialized: false
+}));                                                                  // PASSPORT
+app.use(passport.initialize());                                      // PASSPORT
+app.use(passport.session());                                        // PASSPORT
+
 const dbName = "userDB";
 mongoose.connect("mongodb://localhost:27017/" + dbName, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true); // to avoid deprecation warnings
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);                           // PASSPORT
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());   // adds cookie, encrypts
+passport.deserializeUser(User.deserializeUser());                   // PASSPORT
 
 ///////////////////// Home Route /////////////////////////////////
 
@@ -27,32 +47,14 @@ app.get("/", function(req, res){
   res.render("home.ejs");
 });
 
-///////////////////// Login Route /////////////////////////////////
+///////////////////// Members Route /////////////////////////////////
 
-app.route("/login")
-
-.get(function(req, res){
-  res.render("login.ejs");
-})
-
-.post(function(req, res){
-  User.findOne ( {email: req.body.username}, function(err, foundUser){
-    if (err) {
-      res.send(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(req.body.password, foundUser.password, function(err, result){
-          if (result === true){
-            res.render("members.ejs");
-          } else {
-            res.send("Sorry. Unable to connect to an account with that email and password(WP).");
-          }
-        })
-      } else {
-        res.send("Sorry. Unable to connect to an account with that email and password(WU).");
-      } // end if found User
-    } // end if err
-  });
+app.get("/members", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("members.ejs");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 ///////////////////// Register Route /////////////////////////////////
@@ -64,31 +66,55 @@ app.route("/register")
 })
 
 .post(function(req, res){
-  bcrypt.hash(req.body.password, saltRounds, function(errorNotHandled, hash){
-    let newUser = new User({
-      email: req.body.username,
-      password: hash
-    })
-    User.findOne ( {email: req.body.username}, function(err, uName){
-      if (err) {
-        res.send(err);
-      } else if (uName) {
-        res.send("You have already registered. Please login.")
-      } else {
-        newUser.save(function(error){
-          if(!error){
-            // res.send("You have registered. You may now login.")
-            // res.redirect("/login");
-            // console.log("User registered with u: ", newUser.email, " and p: ", newUser.password);
-            res.render("members.ejs"); // no route for this page so can only reach it by registering
-          } else {
-            res.send(error);
-          }
-        });
-      }
-    }); // end find and save user acc
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.send(err.message);
+      // res.redirect("/register");
+    } else {
+      // check if user not already in DB amd if all ok proceed, store a cookie, hash
+      passport.authenticate("local")(req, res, function(error){
+        if(error){
+          res.send(error);
+        } else {
+          res.redirect("/members");  // let them in if they have registered
+        }
+      })
+    }
   })
+});
 
+///////////////////// Login Route /////////////////////////////////
+
+app.route("/login")
+
+.get(function(req, res){
+  res.render("login.ejs");
+})
+
+.post(function(req, res){
+  const aUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(aUser, function(err){
+    if(err){
+      console.log(err);
+      //return next(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/members");
+      })
+      // return res.redirect("/members" + req.user.username);
+    }
+  })
+});
+
+///////////////////// Logout Route /////////////////////////////////
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
 });
 
 ///////////////////// Listen /////////////////////////////////
